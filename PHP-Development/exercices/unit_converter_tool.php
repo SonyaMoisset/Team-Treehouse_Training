ad3a13b7edb67c7f9423c2bccda5cabd6d7b99c1
Sonya Moisset <sonya.moisset@gmail.com>
2016-09-15 09:41:10 +0100

    Create unit_converter_tool.php

---
+++ PHP-Development/exercices/unit_converter_tool.php
@@ -0,0 +1,20 @@
+<?php 
+$pounds = 140;
+$lb_to_kg = 0.45392;
+$kg = $pounds * $lb_to_kg;
+echo "Weight: ";
+echo $pounds;
+echo " lb = ";
+echo $kg;
+echo " kg";
+
+
+$miles = 2.5;
+$miles_to_km = 1.60934;
+$km = $miles * $miles_to_km;
+echo "Distance: ";
+echo $miles;
+echo " miles = ";
+echo $km;
+echo " km";
+?>
