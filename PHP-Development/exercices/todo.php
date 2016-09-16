161d914c670b34488af0e30e4373c18b25d61f0c
Sonya Moisset <sonya.moisset@gmail.com>
2016-09-16 12:59:05 +0100

    Create todo.php

---
+++ PHP-Development/exercices/todo.php
@@ -0,0 +1,19 @@
+<?php 
+
+  $list['Task 1'] = [
+    'title' => 'Laundry',
+    'priority' => 2,
+    'due' => '',
+    'complete' => true
+  ];
+  $list['Task 2'] = [
+    'title' => 'Clean out refrigerator',
+    'priority' => 3,
+    'due' => '07/30/2016',
+    'complete' => false
+  ];
+  
+  var_dump($list);
+  //echo $list[0]['title'];
+
+?>
