ad2f8f0ea9c4512be21ce53d53cd0599bf37cb02
Sonya Moisset <sonya.moisset@gmail.com>
2016-11-17 14:27:42 +0000

    Create options.py

---
+++ 09-Learn-Flask/exercices/drawing_Bear/options.py
@@ -0,0 +1,56 @@
+DEFAULTS = {
+    "shirts": [
+        "black",
+        "green",
+        "orange",
+        "pink",
+        "red",
+        "teal",
+        "purple"
+    ],
+    "pants": [
+        "black",
+        "blue",
+        "green",
+        "grey",
+        "stars",
+        "stripes",
+        "tiger"
+    ],
+    "hat": [
+        "black-baseball",
+        "black-beanie",
+        "bowler",
+        "headband",
+        "party",
+        "pink-baseball",
+        "pink-beanie"
+    ],
+    "glasses": [
+        "optical-black",
+        "optical-blue",
+        "optical-purple",
+        "optical-red",
+        "optical-yellow",
+        "sun-black",
+        "sun-red"
+    ],
+    "footwear": [
+        "sandal-pink",
+        "sandal-purple",
+        "shoes-black",
+        "shoes-blue",
+        "shoes-grey",
+        "shoes-purple",
+        "shoes-tiger"
+    ],
+    "colors": [
+        "black",
+        "purple",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "red"
+    ]
+}
