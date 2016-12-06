54aae61bae4476f390fe02621304ec68cd6dadbe
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-06 09:25:53 +0000

    Create Map.cs

---
+++ 14-Learn-CSharp/exercises/TreehouseDefenseTowerGame/Map.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace TreehouseDefense
+{
+  class Map
+  {
+    public readonly int Width;
+    public readonly int Height;
+
+    public Map(int width, int height)
+    {
+      Width = width;
+      Height = height;
+    }
+
+    public bool OnMap(Point point)
+    {
+      return point.X >= 0 && point.X < Width &&
+             point.Y >= 0 && point.Y < Height;
+
+      return !(point.X < 0 || point.X >= Width ||
+               point.Y < 0 || point.Y >= Height);
+    }
+  }
+}
