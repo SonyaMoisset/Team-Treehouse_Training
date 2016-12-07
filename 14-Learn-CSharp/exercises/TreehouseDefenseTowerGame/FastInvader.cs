b9d1f2ea748f61182ea3a064e24dda0c23480e0f
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-07 09:13:36 +0000

    Create FastInvader.cs

---
+++ 14-Learn-CSharp/exercises/TreehouseDefenseTowerGame/FastInvader.cs
@@ -0,0 +1,12 @@
+namespace TreehouseDefense
+{
+  class FastInvader : Invader
+  {
+    protected override int StepSize { get; } = 2;
+
+    public override int Health { get; protected set; } = 2;
+
+    public FastInvader(Path path) : base(path)
+    {}
+  }
+}
