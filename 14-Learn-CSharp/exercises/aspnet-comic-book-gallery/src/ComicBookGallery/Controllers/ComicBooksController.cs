10504aa39193d11941f474ffc2d63b8d309b3619
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-16 09:44:34 +0000

    Create ComicBooksController.cs

---
+++ 14-Learn-CSharp/exercises/aspnet-comic-book-gallery/src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,39 @@
+using ComicBookGallery.Data;
+using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+  public class ComicBooksController : Controller
+  {
+    private ComicBookRepository _comicBookRepository = null;
+
+    public ComicBooksController()
+    {
+      _comicBookRepository = new ComicBookRepository();
+    }
+
+    public ActionResult Index()
+    {
+      var comicBooks = _comicBookRepository.GetComicBooks();
+
+      return View(comicBooks);
+    }
+
+    public ActionResult Detail(int? id)
+    {
+      if (id == null)
+      {
+        return HttpNotFound();
+      }
+
+      var comicBook = _comicBookRepository.GetComicBook((int)id);
+
+      return View(comicBook);
+    }
+  }
+}
