8abae2ade6724a2cd9e1ba08242c8246bea1e777
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-16 10:10:15 +0000

    Create ComicBook.cs

---
+++ 14-Learn-CSharp/exercises/aspnet-comic-book-gallery/src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+  public class ComicBook
+  {
+    public int Id { get; set; }
+    public string SeriesTitle { get; set; }
+    public int IssueNumber { get; set; }
+    public string DescriptionHtml { get; set; }
+    public Artist[] Artists { get; set; }
+    public bool Favorite { get; set; }
+
+    public string DisplayText
+    {
+      get
+      {
+        return SeriesTitle + " #" + IssueNumber;
+      }
+    }
+
+    public string CoverImageFileName
+    {
+      get
+      {
+        return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+      }
+    }
+  }
+}
