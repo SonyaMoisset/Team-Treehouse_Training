935df987234f319ed3b37c393d621cab280318a8
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-16 10:07:19 +0000

    Create Artist.cs

---
+++ 14-Learn-CSharp/exercises/aspnet-comic-book-gallery/src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+  public class Artist
+  {
+    public string Name { get; set; }
+    public string Role { get; set; }
+  }
+}
