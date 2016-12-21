9fb3673c5397617bf557a7a23a4ef7150ee57338
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-21 19:59:33 +0000

    Create Counter.js

---
+++ 16-Learn-React/exercises/scoreboard/src/components/Counter.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+
+const Counter = props => (
+  <div ClassName="counter">
+    <button
+      className="counter-action decrement"
+      onClick={() => props.updatePlayerScore(props.index, -1)}>
+      -
+    </button>
+    <div className="counter-score"> {props.score} </div>
+    <button
+      className="counter-action increment"
+      onClick={() => props.updatePlayerScore(props.index, 1)}>
+      +
+    </button>
+  </div>);
+
+  Counter.propTypes = {
+    updatePlayerScore: PropTypes.func.isRequired,
+    index: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired
+  };
+
+export default Counter;
