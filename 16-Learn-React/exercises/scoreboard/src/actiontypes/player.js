e4e6e7ea03f5ee1337daad60773218858fe7e2c2
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-21 19:47:11 +0000

    Create player.js

---
+++ 16-Learn-React/exercises/scoreboard/src/actiontypes/player.js
@@ -0,0 +1,4 @@
+export const ADD_PLAYER = 'player/ADD_PLAYER';
+export const REMOVE_PLAYER = 'player/REMOVE_PLAYER';
+export const UPDATE_PLAYER_SCORE = 'player/UPDATE_PLAYER_SCORE';
+export const SELECT_PLAYER = 'player/SELECT_PLAYER';
