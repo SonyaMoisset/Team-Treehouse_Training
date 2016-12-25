fd901413a81fadf168acd38f9908c89a5c55a5ea
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-25 16:35:23 +0000

    Update Introductions.java

--- 17-Learn-Java/exercises/Introductions.java
+++ 17-Learn-Java/exercises/Introductions.java
@@ -5,7 +5,7 @@ public class Introductions {
     public static void main(String[] args) {
         Console console = System.console();
         // Welcome to the Introductions program!  Your code goes below here
-        String firstName = "Sonya";
+        String firstName = console.readLine("What is your name? ");
         console.printf("Hello, my name is %s\n", firstName);
         console.printf("%s is learning how to write Java\n", firstName);
   }
