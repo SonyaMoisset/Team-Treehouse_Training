6eeeaafdc6ada25567da3509d78255aa575758f9
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-25 21:33:11 +0000

    Update PezDispenser.java

--- 17-Learn-Java/exercises/PezDispenser-retired/PezDispenser.java
+++ 17-Learn-Java/exercises/PezDispenser-retired/PezDispenser.java
@@ -1,8 +1,36 @@
 public class PezDispenser {
+  public static final int MAX_PEZ = 12;
   private String mCharacterName;
+  private int mPezCount;
   
   public PezDispenser(String characterName) {
     mCharacterName = characterName;
+    mPezCount = 0;
+  }
+  
+  public boolean dispense() {
+    boolean wasDispensed = false;
+    if (!isEmpty()) {
+      mPezCount--;
+      wasDispensed = true;
+    }
+    return wasDispensed;
+  }
+  
+  public boolean isEmpty() {
+    return mPezCount == 0;
+  }
+  
+  public void load() {
+    load(MAX_PEZ);
+  }
+  
+  public void load(int pezAmount) {
+    int newAmount = mPezCount + pezAmount;
+    if (newAmount > MAX_PEZ) {
+      throw new IllegalArgumentException("Too many PEZ!!!");
+    }
+    mPezCount = newAmount;
   }
   
   public String getCharacterName() {
