ab74ba4e1247ec69b2c3105db34dfabdb8f3a004
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-25 16:57:11 +0000

    Update TreeStory.java

--- 17-Learn-Java/exercises/TreeStory.java
+++ 17-Learn-Java/exercises/TreeStory.java
@@ -10,9 +10,16 @@ public class TreeStory {
             adjective - A description used to modify or describe a noun
             Enter your amazing code here!
         */
-        String name = console.readLine("Enter your name:   ");
-        String adjective = console.readLine("Enter an adjective:   ");
-        console.printf("%s is very %s", name, adjective);
+      // __Name__ is a __adjective__ __noun__. Theyre are always __adverb__ __verb__.
+      String name = console.readLine("Enter a name: ");
+      String adjective = console.readLine("Enter an adjective: ");
+      String noun = console.readLine("Enter a noun: ");
+      String adverb = console.readLine("Enter an adverb: ");
+      String verb = console.readLine("Enter a verb ending in -ing: ");
+      
+      console.printf("Your TreeStory:\n----------\n");
+      console.printf("%s is a %s %s.   ", name, adjective, noun);
+      console.printf("They are always %s %s.\n", adverb, verb);
     }
     
 }
