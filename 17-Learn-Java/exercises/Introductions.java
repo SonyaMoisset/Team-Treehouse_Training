e5826debc09e37fa16654b4af4276ee2711ef9b7
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-25 16:20:43 +0000

    Create Introductions.java

---
+++ 17-Learn-Java/exercises/Introductions.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+ 
+public class Introductions {
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+        console.printf("Hello, my name is Sonya");
+  }
+}
