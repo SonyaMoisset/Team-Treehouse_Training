b266f5b5ae695b2c675717303c7e200fde30c68c
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-27 13:00:09 +0000

    Create PezDispenser.java

---
+++ 17-Learn-Java/exercises/PezDispenser/PezDispenser.java
@@ -0,0 +1,12 @@
+class PezDispenser {
+  
+  public PezDispenser(String characterName) {
+    this.characterName = characterName;
+  }
+  
+  final private String characterName;
+  
+  public String getCharacterName() {
+    return characterName;
+  }
+}
