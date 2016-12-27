e05be5030f6adf3509a339d1d355e1af311591c7
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-27 23:15:43 +0000

    Update Game.java

--- 17-Learn-Java/exercises/Hangman/Game.java
+++ 17-Learn-Java/exercises/Hangman/Game.java
@@ -1,18 +1,43 @@
-class Game {
+public class Game {
   public static final int MAX_MISSES = 7;
   private String answer;
   private String hits;
   private String misses;
   
   public Game(String answer) {
-    this.answer = answer;
+    this.answer = answer.toLowerCase();
     hits = "";
     misses = "";
   }
   
-  public boolean applyGuess(char letter) {    
-    boolean isHit = answer.indexOf(letter) != -1;
+  public String getAnswer() {
+    return answer;
+  }
+  
+  private char normalizeGuess(char letter) {
+    if (!Character.isLetter(letter)) {
+      throw new IllegalArgumentException("A letter is required");
+    }
+    letter = Character.toLowerCase(letter);
     
+    if (misses.indexOf(letter) != -1 || hits.indexOf(letter) != -1) {
+      throw new IllegalArgumentException(letter + " has already been guessed.");
+    }
+    return letter;
+  }
+  
+  public boolean applyGuess(String letters) {
+    if (letters.length() == 0) {
+      throw new IllegalArgumentException("No letter found");
+    }
+    return applyGuess(letters.charAt(0));
+  }
+  
+  public boolean applyGuess(char letter) {
+    letter = normalizeGuess(letter);
+     
+    boolean isHit = answer.indexOf(letter) != -1;
+
     if (isHit) {
       hits += letter;
     } else {
@@ -36,5 +61,9 @@ class Game {
       progress += display;
     }
     return progress;
+  }
+  
+  public boolean isWon() {
+    return getCurrentProgress().indexOf('-') == -1;
   }
 }
