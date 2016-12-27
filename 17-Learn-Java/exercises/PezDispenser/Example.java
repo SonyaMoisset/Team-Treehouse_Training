dd8c6e71285385edbbd209981057d4977f2ac0d5
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-27 16:18:45 +0000

    Update Example.java

--- 17-Learn-Java/exercises/PezDispenser/Example.java
+++ 17-Learn-Java/exercises/PezDispenser/Example.java
@@ -4,9 +4,44 @@ public class Example {
     
     System.out.println("We are making a new PEZ Dispenser");
     
-    PezDispenser dispenser = new PezDispenser("Yoda");
+    System.out.printf("FUN FACT: There are %d PEZ allowed to every dispenser %n",
+                      PezDispenser.MAX_PEZ);
     
+    PezDispenser dispenser = new PezDispenser("Yoda");
     System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+    
+    if (dispenser.isEmpty()) {
+      System.out.println("Dispenser is empty");
+    }
+    
+    System.out.println("Filling the dispenser with delicious PEZ...");
+    dispenser.fill();
+    
+    if (!dispenser.isEmpty()) {
+      System.out.println("Dispenser is full");
+    }
+    
+    while (dispenser.dispense()) {
+      System.out.println("Chomp!");
+    }
+    
+    if (dispenser.isEmpty()) {
+      System.out.println("Ate all the PEZ");
+    }
+    
+    dispenser.fill(4);
+    dispenser.fill(2);
+    
+    while (dispenser.dispense()) {
+      System.out.println("Chomp!!");
+    }
+    
+    try {
+      dispenser.fill(400);
+      System.out.println("This will never happen");
+    } catch (IllegalArgumentException iae) {
+      System.out.println("Whoa there!");
+      System.out.printf("The error was %s %n", iae.getMessage());
+    }
   }
-
 }
