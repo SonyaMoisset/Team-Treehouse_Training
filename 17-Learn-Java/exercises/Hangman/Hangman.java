f11baad8f32f1f8cbf20907ebf3522fbbc71df88
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-27 23:15:09 +0000

    Update Hangman.java

--- 17-Learn-Java/exercises/Hangman/Hangman.java
+++ 17-Learn-Java/exercises/Hangman/Hangman.java
@@ -2,12 +2,20 @@ public class Hangman {
 
   public static void main(String[] args) {
     
-    Game game = new Game("treehouse");
+    if (args.length == 0) {
+      System.out.println("Usage: java Hangman <answer>");
+      System.err.println("answer is required");
+      System.exit(1);
+    }
+    
+    Game game = new Game(args[0]);
     Prompter prompter = new Prompter(game);
     
-    while (game.getRemainingTries() > 0) {
+    while (game.getRemainingTries() > 0 && !game.isWon()) {
       prompter.displayProgress();
       prompter.promptForGuess();
     }
+    
+    prompter.displayOutcome();
   }
 }
