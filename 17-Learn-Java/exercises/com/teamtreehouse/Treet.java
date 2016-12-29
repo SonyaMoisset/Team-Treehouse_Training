b22baf2c1bb09e9197cd469484616f3cd52e2847
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-29 22:53:16 +0000

    Update Treet.java

--- 17-Learn-Java/exercises/com/teamtreehouse/Treet.java
+++ 17-Learn-Java/exercises/com/teamtreehouse/Treet.java
@@ -1,10 +1,13 @@
 package com.teamtreehouse;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-public class Treet implements Comparable, Serializable {
-  //private boolean mBreakIt = true;
+public class Treet implements Comparable<Treet>, Serializable {
+  private static final long serialVersionUID = 7146681148113043748L;
   private String mAuthor;
   private String mDescription;
   private Date mCreationDate;
@@ -17,15 +20,12 @@ public class Treet implements Comparable, Serializable {
   
   @Override
   public String toString() {
-    return String.format("Treet: \"%s\" by %s on %s",
-                        mDescription,
-                        mAuthor,
-                        mCreationDate);
+    return String.format("Treet:  \"%s\" by %s on %s", 
+                         mDescription, mAuthor, mCreationDate);
   }
   
   @Override
-  public int compareTo(Object obj) {
-    Treet other = (Treet) obj;
+  public int compareTo(Treet other) {
     if (equals(other)) {
       return 0;
     }
@@ -48,7 +48,26 @@ public class Treet implements Comparable, Serializable {
     return mCreationDate;
   }
   
-  public String[] getWords() {
-    return mDescription.toLowerCase().split("[^\\w#@']+");
+  public List<String> getWords() {
+    String[] words = mDescription.toLowerCase().split("[^\\w#@']+"); 
+    return Arrays.asList(words);  
+  }
+  
+  public List<String> getHashTags() {
+    return getWordsPrefixedWith("#");
+  }
+  
+  public List<String> getMentions() {
+    return getWordsPrefixedWith("@");    
+  }
+  
+  private List<String> getWordsPrefixedWith(String prefix) {
+    List<String> results = new ArrayList<String>();
+    for (String word : getWords()) {
+      if (word.startsWith(prefix)) {
+        results.add(word);
+      }
+    }
+    return results;
   }
 }
