3d782ac048f9b826627dd4866ca2ccb4dac935ff
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-30 15:37:44 +0000

    Update Karaoke.java

--- 17-Learn-Java/exercises/karaoke/com/Karaoke.java
+++ 17-Learn-Java/exercises/karaoke/com/Karaoke.java
@@ -7,7 +7,12 @@ public class Karaoke {
   public static void main(String[] args) {
     
     SongBook songBook = new SongBook();
+    songBook.importFrom("songs.txt");
+    
     KaraokeMachine machine = new KaraokeMachine(songBook);
     machine.run();
+    
+    System.out.println("Saving book...");
+    songBook.exportTo("songs.txt");
   }
 }
